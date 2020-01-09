8c87beeffcec2a7a3cefba28a269d1c5f0030d02
Angel Nikoloff Nikoloff <angel.nikoloff@gmail.com>
2020-01-09 10:42:26 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+// Author:		Angel Nikoloff
+// Date:		08-01-20
+// Description:	Simple vanila c++ neural network for XOR solving made with Eigen Library - for education purpose; 
+
+#include "SimpleXorNet_Eigen.h"
+
+int main()
+{
+	anet.run();
+
+	system("pause");
+
+    return 0;
+}
